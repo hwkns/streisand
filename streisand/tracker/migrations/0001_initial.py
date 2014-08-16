cf073f77e338a88cceb60ff0dfed946ce6e8c9c2
Daedax <myth@Ustins-MacBook-Pro.local>
2014-08-16 01:17:50 -0400

    Initial migration

---
+++ streisand/tracker/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Profile',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('auth_key', django_extensions.db.fields.UUIDField(max_length=64, name='auth_key', unique=True, blank=True, editable=False)),
+                ('bytes_uploaded', models.BigIntegerField(default=0)),
+                ('bytes_downloaded', models.BigIntegerField(default=0)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
