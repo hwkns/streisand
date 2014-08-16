43adbf592d2c778befbeee5883f5333a6883fbee
hwkns <danieldhawkins@gmail.com>
2014-08-16 03:49:33 -0400

    segmented INSTALLED_APPS

--- streisand/streisand/settings.py
+++ streisand/streisand/settings.py
@@ -29,15 +29,28 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+DEFAULT_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+)
+
+THIRD_PARTY_APPS = ()
+
+LOCAL_APPS = (
     'tracker',
 )
+
+if DEBUG:
+
+    THIRD_PARTY_APPS += (
+        'django_extensions',
+    )
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
