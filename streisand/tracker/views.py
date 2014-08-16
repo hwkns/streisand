0856520773bffbd30f1da17d59050703d313903c
hwkns <danieldhawkins@gmail.com>
2014-08-16 04:05:45 -0400

    added the beginnings of an outline to the announce view

--- streisand/tracker/views.py
+++ streisand/tracker/views.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from django.views.generic import View
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 class AnnounceView(View):
 
+    VALID_KEYS = {
+        'qqqqqqqqqqqqqqqq',
+        'zzzzzzzzzzzzzzzz',
+    }
+
     def get(self, request, auth_key):
+
+        if auth_key not in self.VALID_KEYS:
+            return HttpResponseNotFound
+
+        # GET params to look for:
+        #   info_hash
+        #   peer_id
+        #   port
+        #   uploaded
+        #   downloaded
+        #   left
+        #   compact
+        #   no_peer_id
+        #   event
+        #   ip
+        #   numwant
+        #   key
+        #   trackerid
+
         return HttpResponse(
             'Hello, world!\n\n'
             '{auth_key}\n\n'
