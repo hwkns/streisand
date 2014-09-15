e7d3caff608a526d5cfebb12db68fd4a707f3023
hwkns <danieldhawkins@gmail.com>
2014-09-15 00:04:25 -0400

    removed tracker initial migration from git -- schema is changing frequently right now

--- streisand/tracker/migrations/0001_initial.py
+++ streisand/tracker/migrations/0001_initial.py
@@ -2,28 +2,50 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 import django_extensions.db.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Profile',
+            name='Peer',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
-                ('auth_key', django_extensions.db.fields.UUIDField(max_length=64, name='auth_key', unique=True, blank=True, editable=False)),
+                ('id', django_extensions.db.fields.UUIDField(serialize=False, primary_key=True, blank=True, name='id', editable=False)),
+                ('user_auth_key', models.CharField(max_length=36)),
+                ('peer_id', models.CharField(max_length=40)),
+                ('ip_address', models.GenericIPAddressField()),
+                ('port', models.CharField(max_length=5)),
                 ('bytes_uploaded', models.BigIntegerField(default=0)),
                 ('bytes_downloaded', models.BigIntegerField(default=0)),
-                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+                ('complete', models.BooleanField(default=False)),
+                ('first_announce', models.DateTimeField(auto_now_add=True)),
+                ('last_announce', models.DateTimeField(auto_now=True)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Swarm',
+            fields=[
+                ('torrent_info_hash', models.CharField(primary_key=True, serialize=False, max_length=40)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='peer',
+            name='swarm',
+            field=models.ForeignKey(to='tracker.Swarm', related_name='peers'),
+            preserve_default=True,
+        ),
+        migrations.AlterUniqueTogether(
+            name='peer',
+            unique_together=set([('swarm', 'user_auth_key', 'ip_address', 'port')]),
         ),
     ]
