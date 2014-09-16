5b4afce6cb8ca7ad8cfa183244d8f1b4487d6809
hwkns <danieldhawkins@gmail.com>
2014-09-16 14:59:06 -0400

    further improvements to the tracker's announce view

--- streisand/tracker/views.py
+++ streisand/tracker/views.py
@@ -1,18 +1,33 @@
 # -*- coding: utf-8 -*-
 
 from urllib.parse import unquote_to_bytes
-from binascii import b2a_hex, a2b_hex
+from binascii import b2a_hex
 import pprint
 
-from django.views.generic import View
-from django.http import HttpResponse
+from django.conf import settings
 from django.core.cache import cache
-from django.utils.timezone import now, timedelta
+from django.http import HttpResponse
+from django.utils.timezone import timedelta
+from django.views.generic import View
 
 from .bencoding import bencode
+from .models import Peer, Swarm
 
 
-ANNOUNCE_INTERVAL_IN_SECONDS = 60 * 40
+class BencodedResponse(HttpResponse):
+    """
+    An HTTP response class that consumes data to be serialized using bencoding.
+    """
+
+    def __init__(self, data, **kwargs):
+        kwargs.setdefault('content_type', 'text/plain')
+        data = bencode(data)
+        super(BencodedResponse, self).__init__(content=data, **kwargs)
+
+
+cache.set('announce_interval_in_seconds', timedelta(minutes=40))
+announce_interval_timedelta = cache.get('announce_interval_in_seconds')
+ANNOUNCE_INTERVAL_IN_SECONDS = int(announce_interval_timedelta.total_seconds())
 
 PEER_ID_WHITELIST = (
     b'-AZ25',
@@ -98,67 +113,16 @@ PEER_ID_WHITELIST = (
     b'-AZ53',
     b'-UT34',
 )
-
-
-class Peer(object):
-
-    # TODO: put completed and uncompleted peers in separate lists under
-    # the torrent so it's quicker to find the length of them
-
-    def __init__(self, ip, port, last_modified, uploaded=0, downloaded=0, completed=False):
-        self.ip = ip
-        self.port = port
-        self.last_modified = last_modified
-        self.uploaded = uploaded
-        self.downloaded = downloaded
-        self.completed = completed
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __str__(self):
-        return '{ip}:{port}'.format(
-            ip=self.ip,
-            port=self.port,
-        )
-
-
-def encode_peer(ip, port):
-    """
-    Encodes a peer string (e.g., '127.0.0.1:8000') into a
-    compact byte representation (e.g., b'\x7f\x00\x00\x01\x1f@')
-    """
-    compact_peer = bytes([int(s) for s in ip.split('.')])
-    compact_peer += int(port).to_bytes(2, byteorder='big')
-    return compact_peer
-
-
-TORRENTS = {
-    b2a_hex(b'\x89I\x85\xf9|\xc2\\$n7\xa0|\xd7\xc7\x85\x999\x82\xa7\xcb'): {
-        encode_peer(ip='115.231.38.54', port='20302'): Peer(
-            ip='115.231.38.54',
-            port='20302',
-            last_modified=now(),
-            completed=True,
-        ),
-        encode_peer(ip='114.248.24.87', port='17099'): Peer(
-            ip='114.248.24.87',
-            port='17099',
-            last_modified=now(),
-            completed=True,
-        ),
-    },
-}
+PEER_ID_WHITELIST = tuple(
+    [
+        b2a_hex(prefix).decode('ascii')
+        for prefix
+        in PEER_ID_WHITELIST
+    ]
+)
 
 USERS = {
-    '16fd2706-8baf-433b-82eb-8c7fada847da': {
-        'username': 'hwkns',
-        'torrents': {
-            b2a_hex(b'\x89I\x85\xf9|\xc2\\$n7\xa0|\xd7\xc7\x85\x999\x82\xa7\xcb'),
-        },
-        'uploaded': 0,
-        'downloaded': 0,
-    }
+    '16fd2706-8baf-433b-82eb-8c7fada847da',
 }
 
 
@@ -180,8 +144,7 @@ class AnnounceView(View):
         #
 
         # Fail if the auth_key is invalid
-        user = USERS.get(auth_key)
-        if not user:
+        if auth_key not in USERS:
             return self.failure('Invalid auth_key')
 
         # Fail if the client will not accept a compact response
@@ -203,17 +166,19 @@ class AnnounceView(View):
         except Exception:
             return self.failure('Announce request contained malformed GET parameters')
         else:
-            info_hash = unquote_to_bytes(params['info_hash'])
-            peer_id = unquote_to_bytes(params['peer_id'])
+            info_hash = b2a_hex(unquote_to_bytes(params['info_hash'])).decode('ascii')
+            peer_id = b2a_hex(unquote_to_bytes(params['peer_id'])).decode('ascii')
 
         # Fail if the client is not in the whitelist
         if not peer_id.startswith(PEER_ID_WHITELIST):
             return self.failure('Your client is not in the whitelist')
 
         # Fail if the torrent is not registered
-        peers = TORRENTS.get(b2a_hex(info_hash))
-        if not peers:
-            return self.failure('Unregistered torrent')
+        try:
+            torrent = Swarm.objects.get(torrent_info_hash=info_hash)
+        except Swarm.DoesNotExist:
+            torrent = Swarm.objects.create(torrent_info_hash=info_hash)
+            # return self.failure('Unregistered torrent')
 
         #
         # Get announce data
@@ -221,18 +186,21 @@ class AnnounceView(View):
 
         # The client's IP address
         ip = request.META['REMOTE_ADDR']
+        # In debug mode, allow it to be specified as a GET param
+        if settings.DEBUG:
+            ip = request.GET.get('ip', ip)
 
         # The port number that the client is listening on
         port = request.GET['port']
 
         # The total number of bytes uploaded since the first 'started' event
-        uploaded = int(request.GET['uploaded'])
+        bytes_uploaded = int(request.GET['uploaded'])
 
         # The total number of bytes downloaded since the first 'started' event
-        downloaded = int(request.GET['downloaded'])
+        bytes_downloaded = int(request.GET['downloaded'])
 
         # The number of bytes remaining until 100% completion
-        left = int(request.GET['left'])
+        bytes_left = int(request.GET['left'])
 
         # Either 'started', 'completed', or 'stopped' (optional)
         event = request.GET.get('event')
@@ -245,7 +213,7 @@ class AnnounceView(View):
         #    no_peer_id - We always send compact responses
         #
         #    ip - No proxy announcing allowed; we will distribute
-        #        the IP from which this request originated
+        #        the IP address from which this request originated
         #
         #    trackerid - Most clients don't implement this, and we
         #        have no use for it
@@ -256,42 +224,64 @@ class AnnounceView(View):
         # print(params)
         # print(cache.keys('*'))
 
-        # Add the client to the peer list
-        client = encode_peer(ip=ip, port=port)
-        if event == 'stopped':
-            if client in peers:
-                del peers[client]
-        else:
-            if client in peers:
-                # Update this client's peer list entry
-                peer = peers[client]
-                if peer.downloaded < downloaded:
-                    # TODO: async update the user's total downloaded amount
-                    pass
-                if peer.uploaded < uploaded:
-                    # TODO: async update the user's total uploaded amount
-                    peer.uploaded = uploaded
-            else:
-                # Add this client to the peer list
-                peers[client] = Peer(
-                    ip=ip,
-                    port=port,
-                    last_modified=now(),
-                    uploaded=uploaded,
-                    downloaded=downloaded,
-                    completed=(event == 'completed') or (left == 0),
-                )
+        #
+        # Update the peer's stats, and send signals
+        #
 
-        # Get the peer list to send back to the client
-        compact_peer_list_for_client = b''.join(
-            [peer for peer in peers.keys()][:num_want]
-        )
-        assert len(compact_peer_list_for_client) % 6 == 0
+        try:
+            client = torrent.peers.get(ip_address=ip, port=port)
+
+        except Peer.DoesNotExist:
+
+            # Add this client to the peer list
+            client = Peer.objects.create(
+                swarm=torrent,
+                ip_address=ip,
+                port=port,
+                peer_id=peer_id,
+                complete=(event == 'completed') or (bytes_left == 0),
+            )
+
+        if bytes_downloaded > client.bytes_downloaded:
+            # TODO: put a message in the queue that this auth_key downloaded
+            # (bytes_downloaded - client.bytes_downloaded) bytes on this torrent_hash
+            client.bytes_downloaded = bytes_downloaded
+
+        if bytes_uploaded > client.bytes_uploaded:
+            # TODO: put a message in the queue that this auth_key uploaded
+            # (bytes_uploaded - client.bytes_uploaded) bytes on this torrent_hash
+            client.bytes_uploaded = bytes_uploaded
+
+        if event == 'started':
+            # TODO: put a 'started' message in the queue
+            pass
+        elif event == 'stopped':
+            # TODO: put a 'stopped' message in the queue
+            pass
+        elif event == 'completed':
+            # TODO: put a 'snatched' message in the queue
+            pass
+
+        #
+        # Respond to the client
+        #
+
+        # Get the peer list to send back
+        if event == 'stopped':
+            client.delete()
+            compact_peer_list_for_client = b''
+        else:
+            client.save()
+            compact_peer_list_for_client = b''.join(
+                [bytes(peer) for peer in torrent.peers.all()][:num_want]
+            )
+            assert len(compact_peer_list_for_client) % 6 == 0
 
         # Get the number of seeders and leechers
-        complete = len([peer for peer in peers if peers[peer].completed])
-        incomplete = len(peers) - complete
+        complete = torrent.peers.filter(complete=True).count()
+        incomplete = torrent.peers.filter(complete=False).count()
 
+        # Put everything in a dictionary
         response_dict = {
             'interval': ANNOUNCE_INTERVAL_IN_SECONDS,
             'min interval': ANNOUNCE_INTERVAL_IN_SECONDS,
@@ -300,28 +290,32 @@ class AnnounceView(View):
             'peers': compact_peer_list_for_client,
         }
 
-        return HttpResponse(
-            'Auth key: {auth_key}<br/>'
-            'User: {user}<br/>'
-            'IP: {ip}<br/>'
-            '<br/>Torrent: <pre>{torrent}</pre><br/>'
-            '<br/>Params: <pre>{params}</pre><br/>'
-            '<br/>Response: <pre>{response_dict}</pre><br/>'.format(
-                auth_key=auth_key,
-                user=user,
-                ip=ip,
-                torrent=peers,
-                params=pprint.pformat(params),
-                response_dict=pprint.pformat(response_dict),
+        if settings.DEBUG:
+            return HttpResponse(
+                'Auth key: {auth_key}<br/>'
+                'IP: {ip}<br/>'
+                '<br/>Torrent: {torrent}<br/>'
+                '<br/>Peers: {peers}<br/>'
+                '<br/>Request params: <pre>{params}</pre><br/>'
+                '<br/>Response: <pre>{response_dict}</pre><br/>'.format(
+                    auth_key=auth_key,
+                    ip=ip,
+                    torrent=torrent,
+                    peers=torrent.peers.all(),
+                    params=pprint.pformat(params),
+                    response_dict=pprint.pformat(response_dict),
+                )
             )
-        )
+        else:
+            return BencodedResponse(response_dict)
 
     @staticmethod
     def failure(reason):
-        return HttpResponse(
-            bencode(
-                {
-                    'failure reason': reason
-                }
-            )
+        """
+        Send a failure response to the client with the given reason.
+        """
+        return BencodedResponse(
+            {
+                'failure reason': reason
+            }
         )
