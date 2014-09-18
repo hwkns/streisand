1856e85c267bc7e0ae801276698b1bc3a8797328
hwkns <danieldhawkins@gmail.com>
2014-09-18 11:41:18 -0400

    updated tracker tests (using dev fixture for now)

--- streisand/tracker/tests.py
+++ streisand/tracker/tests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from unittest.mock import patch
+from unittest.mock import patch, ANY
 
 from django_dynamic_fixture import G
 
@@ -9,19 +9,22 @@ from django.test import TestCase, RequestFactory
 from profiles.models import UserProfile
 from tracker.models import Swarm
 from tracker.views import AnnounceView
+from tracker.utils import unquote_to_hex
 
 
 @patch('tracker.views.handle_announce.delay')
 class AnnounceHandlerTests(TestCase):
 
+    fixtures = ['dev.json']
+
     def setUp(self):
         self.factory = RequestFactory()
         self.announce_view = AnnounceView.as_view()
         self.swarm = G(Swarm, torrent_info_hash='894985f97cc25c246e37a07cd7c785993982a7cb')
-        self.profile = G(UserProfile, auth_key='16fd2706-8baf-433b-82eb-8c7fada847da')
+        self.profile = UserProfile.objects.get(auth_key='16fd2706-8baf-433b-82eb-8c7fada847da')
         self.announce_data = {
             'info_hash': '%89I%85%F9%7C%C2%5C%24n7%A0%7C%D7%C7%85%999%82%A7%CB',
-            'peer_id': '-UT3400-111122221111',
+            'peer_id': '-DE1360-m8vgv0uzHUF0',
             'uploaded': '422',
             'downloaded': '381',
             'left': '0',
@@ -33,12 +36,29 @@ class AnnounceHandlerTests(TestCase):
         Hack so we can use a dict with pre-urlencoded data (info_hash bytes)
         """
         params = '&'.join('='.join([k, v]) for k, v in announce_data.items())
-        return self.factory.get(path='/?{params}'.format(params=params))
+        return self.factory.get(path='/?{params}'.format(params=params), HTTP_USER_AGENT='foobar')
 
     def test_announce_handler_called__proper_announce(self, handler_mock):
         request = self.announce_request(announce_data=self.announce_data)
         self.announce_view(request, auth_key=self.profile.auth_key)
         self.assertTrue(handler_mock.called)
+
+    def test_announce_handler_called_with_correct_arguments(self, handler_mock):
+        request = self.announce_request(announce_data=self.announce_data)
+        self.announce_view(request, auth_key=self.profile.auth_key)
+        handler_mock.assert_called_once_with(
+            auth_key=self.profile.auth_key,
+            info_hash=self.swarm.torrent_info_hash,
+            new_bytes_uploaded=422,
+            new_bytes_downloaded=381,
+            bytes_remaining=0,
+            event='',
+            ip_address='127.0.0.1',
+            port=1337,
+            peer_id=unquote_to_hex('-DE1360-m8vgv0uzHUF0'),
+            user_agent='foobar',
+            time_stamp=ANY,
+        )
 
     def test_announce_handler_not_called__bad_auth_key(self, handler_mock):
         request = self.announce_request(announce_data=self.announce_data)
