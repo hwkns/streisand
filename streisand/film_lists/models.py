55482f46bac8664ddbda90e3974122f2afb5b84d
hwkns <danieldhawkins@gmail.com>
2014-09-22 01:23:23 -0400

    oops

--- streisand/film_lists/models.py
+++ streisand/film_lists/models.py
@@ -19,7 +19,7 @@ class FilmList(models.Model):
 class FilmListItem(models.Model):
     film = models.ForeignKey('films.Film')
     film_list = models.ForeignKey('film_lists.FilmList')
-    sequence_number = models.IntegerField
+    sequence_number = models.IntegerField()
 
     class Meta:
         unique_together = ['film_list', 'sequence_number']
