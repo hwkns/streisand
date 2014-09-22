4fa8c8d55cb4909c902d1a18413c3eb057ae4087
hwkns <danieldhawkins@gmail.com>
2014-09-22 01:44:10 -0400

    added film_lists app to www urls

--- streisand/www/urls.py
+++ streisand/www/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', 'www.views.home', name='home'),
     url(r'^films/', include('films.urls')),
+    url(r'^film-lists/', include('film_lists.urls')),
     url(r'^profiles/', include('profiles.urls')),
     url(r'^torrents/', include('torrents.urls')),
     url(r'^admin/', include(admin.site.urls)),
