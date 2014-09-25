cf3a8df6f799f2158b6f6914e10d776fff16482e
hwkns <danieldhawkins@gmail.com>
2014-09-24 23:48:06 -0400

    removed Swarm query from home page

--- streisand/www/views.py
+++ streisand/www/views.py
@@ -2,14 +2,11 @@
 
 from django.shortcuts import render
 
-from tracker.models import Swarm
-
 
 def home(request):
     return render(
         request=request,
         template_name='home.html',
         dictionary={
-            'swarms': Swarm.objects.all(),
         }
     )
