ba99568d810e7417ad7f36cf25c430b1beb7f27d
hwkns <danieldhawkins@gmail.com>
2014-10-07 01:31:31 -0400

    use order_with_respect_to for FilmList ordering

--- streisand/film_lists/models.py
+++ streisand/film_lists/models.py
@@ -7,19 +7,30 @@ from django.db import models
 class FilmList(models.Model):
     title = models.CharField(max_length=1024)
     description = models.TextField()
-    films = models.ManyToManyField(to='films.Film', through='film_lists.FilmListItem')
+    films = models.ManyToManyField(
+        to='films.Film',
+        related_name='film_lists',
+        through='film_lists.FilmListItem',
+    )
 
     def __str__(self):
         return self.title
+
+    def __len__(self):
+        return self.films.count()
 
     def get_absolute_url(self):
         return reverse('film_list_details', args=[self.id])
 
 
 class FilmListItem(models.Model):
+    film_list = models.ForeignKey(
+        'film_lists.FilmList',
+        related_name='items',
+        db_index=True,
+    )
     film = models.ForeignKey('films.Film')
-    film_list = models.ForeignKey('film_lists.FilmList')
-    sequence_number = models.IntegerField()
 
     class Meta:
-        unique_together = ['film_list', 'sequence_number']
+        unique_together = ['film_list', 'film']
+        order_with_respect_to = 'film_list'
