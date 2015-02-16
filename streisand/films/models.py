1b04f9d76d655381f8d91330ea8569325c25a13d
hwkns <danieldhawkins@gmail.com>
2015-02-16 03:48:42 -0500

    added a year field to the Film model

--- streisand/films/models.py
+++ streisand/films/models.py
@@ -5,13 +5,15 @@ from django.db import models
 
 
 class Film(models.Model):
+
     title = models.CharField(max_length=1024)
+    year = models.PositiveSmallIntegerField()
     imdb_id = models.CharField(max_length=9)
     tmdb_id = models.IntegerField()
     poster_url = models.URLField()
     fanart_url = models.URLField()
-    trailer_type = models.CharField(max_length=64)
     trailer_url = models.URLField()
+    trailer_type = models.CharField(max_length=64)
     duration_in_minutes = models.IntegerField()
     description = models.TextField()
 
@@ -20,3 +22,9 @@ class Film(models.Model):
 
     def get_absolute_url(self):
         return reverse('film_details', args=[self.id])
+
+    @staticmethod
+    def autocomplete_search_fields():
+        return (
+            "title__icontains",
+        )
