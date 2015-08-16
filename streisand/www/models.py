907adf77cf009a41ea985b0d031cffde9ae79024
hwkns <danieldhawkins@gmail.com>
2015-08-16 03:49:50 -0400

    added LogEntry model

--- streisand/www/models.py
+++ streisand/www/models.py
@@ -12,3 +12,18 @@ class Feature(models.Model):
     is_enabled = models.BooleanField(default=False)
 
     objects = FeatureManager()
+
+
+class LogEntry(models.Model):
+
+    ACTION_CHOICES = (
+        'torrent_uploaded',
+        'torrent_edited',
+        'torrent_deleted',
+        'user_created',
+        'user_deleted',
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    action = models.CharField(max_length=128, db_index=True)
+    text = models.CharField(max_length=1024)
