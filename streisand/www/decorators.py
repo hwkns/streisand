4d23e62dcd43cb26837bbaa90df8c516f236940e
hwkns <danieldhawkins@gmail.com>
2017-10-19 03:28:08 -0400

    class based view decorator

--- streisand/www/decorators.py
+++ streisand/www/decorators.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from functools import wraps
+from functools import wraps, update_wrapper
 
 from django.conf import settings
 from django.http import HttpResponseRedirect
-from django.utils.decorators import available_attrs
+from django.utils.decorators import method_decorator, available_attrs
+from django.views.generic import View
 
 
 def https_required(view_func):
@@ -22,3 +23,22 @@ def https_required(view_func):
             secure_url = url.replace('http://', 'https://')
             return HttpResponseRedirect(secure_url)
     return _wrapped_view
+
+
+def class_based_view_decorator(decorator):
+    """
+    Converts a function decorator into a class-based view decorator.
+
+    Converts the function decorator into a method decorator, then
+    applies that method decorator to the view's dispatch method.
+    """
+    def _dec(cls):
+        assert (isinstance(cls, type) and issubclass(cls, View)), (
+            "Only subclasses of django.views.generic.View may use this decorator."
+        )
+        _method_decorator = method_decorator(decorator)
+        cls.dispatch = _method_decorator(cls.dispatch)
+        return cls
+
+    update_wrapper(_dec, decorator, assigned=available_attrs(decorator))
+    return _dec
