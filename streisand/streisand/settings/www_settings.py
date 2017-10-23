ed70e4a996087fee14cefbe5a09be2576a76cf4d
hwkns <danieldhawkins@gmail.com>
2017-10-22 23:33:17 -0400

    camelCase JSON parsing and rendering for the API

--- streisand/streisand/settings/www_settings.py
+++ streisand/streisand/settings/www_settings.py
@@ -73,6 +73,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
 }
 
 if DEBUG:
