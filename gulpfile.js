f2d6bcdfa0fc98d4aa7095926793c694c9f913cf
unitPower <unitpowers@gmail.com>
2017-10-26 20:54:00 -0700

    Update dev-server

--- gulpfile.js
+++ gulpfile.js
@@ -16,15 +16,7 @@ gulp.task('hot-load', function (callback) {
         historyApiFallback: true,
         quiet: false,
         noInfo: false,
-        stats: {
-            assets: false,
-            colors: true,
-            version: false,
-            hash: false,
-            timings: false,
-            chunks: false,
-            chunkModules: false
-        }
+        stats: 'minimal'
     }).listen(3000, 'localhost', function (err) {
         if (err) throw new gutil.PluginError('webpack-dev-server', err);
         gutil.log('[webpack-dev-server]', 'http://localhost:3000/webpack-dev-server/index.html');
