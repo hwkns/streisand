9e4665c74daedfc7c3e19fdc18ac360bda59f4c7
unitPower <unitpowers@gmail.com>
2017-10-28 13:02:37 -0700

    Fixes #2 - Store auth token in session storage

--- src/reducers/auth.ts
+++ src/reducers/auth.ts
@@ -3,16 +3,29 @@ import * as assign from 'object-assign';
 import IAuthInfo from '../models/IAuthInfo';
 import Action from '../actions/AuthAction';
 
+let authToken: string;
+if (typeof sessionStorage !== 'undefined') {
+    authToken = sessionStorage['jumpcut.token'] || undefined;
+}
+
 const defaultValue: IAuthInfo = {
-    isAuthenticated: false,
-    isAuthenticating: false
+    isAuthenticated: !!authToken,
+    isAuthenticating: false,
+    token: authToken
 };
+
+function storeToken(token: string) {
+    if (typeof sessionStorage !== 'undefined') {
+        sessionStorage['jumpcut.token'] = token;
+    }
+}
 
 function auth(state: IAuthInfo = defaultValue, action: Action): IAuthInfo {
     switch (action.type) {
         case 'AUTHENTICATING':
             return assign({}, state, { isAuthenticating: true });
         case 'AUTHENTICATED':
+            storeToken(action.token);
             return assign({}, state, {
                 isAuthenticated: true,
                 isAuthenticating: false,
