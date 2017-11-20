0cf8a625c596f16858066bb78a9d9d0c88a5db3e
unitPower <unitpowers@gmail.com>
2017-11-19 19:17:27 -0800

    Add basic bbcode handler for torrent descriptions

--- src/components/torrents/TorrentDetails.tsx
+++ src/components/torrents/TorrentDetails.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import { Parser } from 'bbcode-to-react';
 import { Tabs, Tab } from 'react-bootstrap';
 
 import IFilm from '../../models/IFilm';
 import ITorrent from '../../models/ITorrent';
+
+const parser = new Parser();
 
 export type Props = {
     film: IFilm;
@@ -13,7 +16,7 @@ export default function TorrentDetails(props: Props) {
     const torrent = props.torrent;
     return (
         <Tabs defaultActiveKey={2}>
-            {/* <Tab eventKey={1} title="Description">Tab 1 content</Tab> */}
+            <Tab eventKey={1} title="General"><GeneralContent torrent={torrent} /></Tab>
             <Tab eventKey={2} title="Video"><MediaContent torrent={torrent} /></Tab>
             <Tab eventKey={3} title="Torrent"><TorrentContent torrent={torrent} /></Tab>
         </Tabs>
@@ -30,6 +33,14 @@ function InfoRow(props: IRowProps) {
         <li className="list-group-item">
             <strong>{props.label}</strong>: <span className="text-muted">{props.value}</span>
         </li>
+    );
+}
+
+function GeneralContent(props: { torrent: ITorrent }) {
+    return (
+        <div style={{ 'margin-top': '8px' }}>
+            <div>{parser.toReact(props.torrent.description)}</div>
+        </div>
     );
 }
 
