a1b593a1d8e80428cce7ed0359834621305ac83b
saltman <saltman@dev.tehjump.xyz>
2018-03-25 14:46:17 +0200

    update filters for forums per latest filter release

--- streisand/forums/filters.py
+++ streisand/forums/filters.py
@@ -4,9 +4,9 @@ from .models import ForumTopic, ForumThread, ForumPost
 
 class ForumTopicFilter(filters.FilterSet):
 
-    name = filters.CharFilter(name='name', lookup_expr='icontains')
-    description = filters.CharFilter(name='description', lookup_expr='icontains')
-    latest_post_author = filters.CharFilter(name='latest_post__author__username', lookup_expr='icontains')
+    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+    description = filters.CharFilter(field_name='description', lookup_expr='icontains')
+    latest_post_author = filters.CharFilter(field_name='latest_post__author__username', lookup_expr='icontains')
 
     class Meta:
         model = ForumTopic
@@ -15,9 +15,9 @@ class ForumTopicFilter(filters.FilterSet):
 
 class ForumThreadFilter(filters.FilterSet):
 
-    title = filters.CharFilter(name='title', lookup_expr='icontains')
-    created_at = filters.DateRangeFilter(name='created_at', lookup_expr='range')
-    created_by = filters.CharFilter(name='created_by', lookup_expr='icontains')
+    title = filters.CharFilter(field_name='title', lookup_expr='icontains')
+    created_at = filters.DateRangeFilter(field_name='created_at', lookup_expr='range')
+    created_by = filters.CharFilter(field_name='created_by', lookup_expr='icontains')
 
     class Meta:
         model = ForumThread
@@ -26,9 +26,9 @@ class ForumThreadFilter(filters.FilterSet):
 
 class ForumPostFilter(filters.FilterSet):
 
-    body = filters.CharFilter(name='body', lookup_expr='icontains')
-    created_at = filters.DateRangeFilter(name='created_at', lookup_expr='range')
-    author = filters.CharFilter(name='author', lookup_expr='icontains')
+    body = filters.CharFilter(field_name='body', lookup_expr='icontains')
+    created_at = filters.DateRangeFilter(field_name='created_at', lookup_expr='range')
+    author = filters.CharFilter(field_name='author', lookup_expr='icontains')
 
     class Meta:
         model = ForumPost
