9d536b79bd857aeda124663b18d8d3b1428c6cde
saltman <saltman@dev.tehjump.xyz>
2018-03-30 12:15:52 +0200

    fix renderers 	modified:   streisand/www/renderers.py

--- streisand/www/renderers.py
+++ streisand/www/renderers.py
@@ -1,33 +1,10 @@
 # -*- coding: utf-8 -*-
-
-from rest_framework.renderers import JSONRenderer
-
-
-# http://stackoverflow.com/a/19053800
-def to_camel_case(snake_str):
-    components = snake_str.split('_')
-    # We capitalize the first letter of each component except the first one
-    # with the 'title' method and join them together.
-    return components[0] + "".join(x.title() for x in components[1:])
+from rest_framework import renderers
+from www.util import camelize
 
 
-def camel_case_data(data):
-
-    if isinstance(data, list):
-        return [camel_case_data(each) for each in data]
-
-    if isinstance(data, dict):
-        return {to_camel_case(key): camel_case_data(value) for key, value in data.items()}
-
-    return data
-
-
-class CamelCaseJSONRenderer(JSONRenderer):
-    def render(self, data, accepted_media_type=None, renderer_context=None):
-        data = camel_case_data(data)
-
+class CamelCaseJSONRenderer(renderers.JSONRenderer):
+    def render(self, data, *args, **kwargs):
         return super(CamelCaseJSONRenderer, self).render(
-            data=data,
-            accepted_media_type=accepted_media_type,
-            renderer_context=renderer_context
+            camelize(data), *args, **kwargs
         )
