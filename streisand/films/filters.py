6ccc4bd3047f859650d29db6f66b869256e3cfbc
saltman <saltman@jumpcut.to>
2018-04-06 00:49:01 -0400

    fix film filters

--- streisand/films/filters.py
+++ streisand/films/filters.py
@@ -7,7 +7,6 @@ class FilmFilter(filters.FilterSet):
     title = filters.CharFilter(field_name='title', lookup_expr='icontains')
     description = filters.CharFilter(field_name='description', lookup_expr='icontains')
     tags = filters.CharFilter(field_name='tags')
-    year = filters.DateFilter(field_name='creator__username', lookup_expr='lt')
 
     class Meta:
         model = Film
@@ -19,7 +18,7 @@ class CollectionFilter(filters.FilterSet):
     title = filters.CharFilter(field_name='title', lookup_expr='icontains')
     description = filters.CharFilter(field_name='description', lookup_expr='icontains')
     film__title = filters.CharFilter(field_name='film__title', lookup_expr='icontains')
-    creator__username = filters.CharFilter(field_name='creator__username', lookup_expr='iexact')
+    creator__username = filters.CharFilter(field_name='creator__username', lookup_expr='icontains')
 
     class Meta:
         model = Collection
