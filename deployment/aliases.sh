a250b86974e384f19a0fb12b87ea0d7fe5f263d0
noxshade <noxshade@example.com>
2018-04-24 19:59:44 +0100

    Update aliases to be more useful

--- deployment/aliases.sh
+++ deployment/aliases.sh
@@ -1,10 +1,14 @@
-alias start_containers='docker-compose up -d'
-alias stop_containers='docker-compose down'
-alias pull_containers='docker-compose pull'
-alias deploy_updates='bash deploy_updates.sh'
-alias logs='docker-compose logs -f'
+alias _cd_deployment='cd /code/JumpCut/deployment'
+alias start_containers='(_cd_deployment && docker-compose up -d)'
+alias stop_containers='(_cd_deployment && docker-compose down)'
+alias pull_containers='(_cd_deployment && docker-compose pull)'
+alias deploy_updates='(_cd_deployment && bash deploy_updates.sh)'
+alias logs='(_cd_deployment && docker-compose logs -f)'
 
-alias m='docker-compose run --rm www src/manage.py'
-alias shell='m shell_plus --settings=streisand.settings.www_settings'
+function m() {
+    (_cd_deployment && docker-compose run --rm www src/manage.py "$@")
+}
+
+alias shell='m shell_plus'
 alias clean_slate='m reset_db --noinput && m migrate && m loaddata foundation'
 alias fixtures='m loaddata dev'
